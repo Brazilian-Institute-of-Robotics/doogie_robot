29a2732595a54a892e1585452373f766c426c2ef
Mateus Menezes <mateusmenezes95@gmail.com>
2019-11-11 17:27:23 -0300

    Improve encode readings and documentation

--- doogie_drivers/src/quadrature_encoder_driver.cpp
+++ doogie_drivers/src/quadrature_encoder_driver.cpp
@@ -36,7 +36,7 @@ volatile int QuadratureEncoder::pulses_[2] = {0, 0};
 uint8_t QuadratureEncoder::prev_state_[2] = {0, 0};
 uint8_t QuadratureEncoder::curr_state_[2] = {0, 0};
 
-QuadratureEncoder::Encoding QuadratureEncoder::encoding_ = QuadratureEncoder::Encoding::X4_ENCODING;
+QuadratureEncoder::Encoding QuadratureEncoder::encoding_ = QuadratureEncoder::Encoding::X2_ENCODING;
 
 QuadratureEncoder::QuadratureEncoder(unsigned int pulses_per_rev, double wheel_radius, unsigned int gear_ratio) {
   this->pulses_per_rev_ = pulses_per_rev * gear_ratio;
@@ -96,50 +96,56 @@ double QuadratureEncoder::getLinearPosition(EncoderSide enc_side) {
   lin_poisition *= (1.0f / pulses_per_meters_);
   return lin_poisition;
 }
-// +-------------+
-// | X2 Encoding |
-// +-------------+
-//
-// When observing states two patterns will appear:
-//
-// Counter clockwise rotation:
-//
-// 10 -> 01 -> 10 -> 01 -> ...
-//
-// Clockwise rotation:
-//
-// 11 -> 00 -> 11 -> 00 -> ...
-//
-// We consider counter clockwise rotation to be "forward" and
-// counter clockwise to be "backward". Therefore pulse count will increase
-// during counter clockwise rotation and decrease during clockwise rotation.
-//
-// +-------------+
-// | X4 Encoding |
-// +-------------+
-//
-// There are four possible states for a quadrature encoder which correspond to
-// 2-bit gray code.
-//
-// A state change is only valid if of only one bit has changed.
-// A state change is invalid if both bits have changed.
-//
-// Clockwise Rotation ->
-//
-//    00 01 11 10 00
-//
-// <- Counter Clockwise Rotation
-//
-// If we observe any valid state changes going from left to right, we have
-// moved one pulse clockwise [we will consider this "backward" or "negative"].
-//
-// If we observe any valid state changes going from right to left we have
-// moved one pulse counter clockwise [we will consider this "forward" or
-// "positive"].
-//
-// We might enter an invalid state for a number of reasons which are hard to
-// predict - if this is the case, it is generally safe to ignore it, update
-// the state and carry on, with the error correcting itself shortly after.
+
+/** +-------------+
+ * | X2 Encoding |
+ * +-------------+
+ *
+ * When observing states two patterns will appear:
+ *
+ * Counter clockwise rotation:
+ *
+ * 10 -> 01 -> 10 -> 01 -> ...
+ *
+ * Clockwise rotation:
+ *
+ * 11 -> 00 -> 11 -> 00 -> ...
+ *
+ * We consider counter clockwise rotation to be "forward" and
+ * counter clockwise to be "backward". Therefore pulse count will increase
+ * during counter clockwise rotation and decrease during clockwise rotation.
+ *
+ * +-------------+
+ * | X4 Encoding |
+ * +-------------+
+ *
+ * There are four possible states for a quadrature encoder which correspond to
+ * 2-bit gray code.
+ *
+ * A state change is only valid if of only one bit has changed.
+ * A state change is invalid if both bits have changed.
+ *
+ * Clockwise Rotation ->
+ *
+ *    00 01 11 10 00
+ *
+ * <- Counter Clockwise Rotation
+ *
+ * If we observe any valid state changes going from left to right, we have
+ * moved one pulse clockwise [we will consider this "backward" or "negative"].
+ *
+ * If we observe any valid state changes going from right to left we have
+ * moved one pulse counter clockwise [we will consider this "forward" or
+ * "positive"].
+ *
+ * We might enter an invalid state for a number of reasons which are hard to
+ * predict - if this is the case, it is generally safe to ignore it, update
+ * the state and carry on, with the error correcting itself shortly after.
+ * 
+ * Note that these concepts apply to the left engine. The right engine is
+ * mirrored relative to the left. Thus, the direction of rotation is reverse
+ * and the encoder reading code of both motors differs slightly. 
+ */
 void QuadratureEncoder::encodeLeft() {
   int change = 0;
   uint8_t channel_a_state = digitalRead(channel_pins_[LEFT_ENC_CHA]);
@@ -147,12 +153,12 @@ void QuadratureEncoder::encodeLeft() {
 
   curr_state_[LEFT_ENC] = (channel_a_state << 1) | (channel_b_state);  // 2-bit state.
   if (encoding_ == X2_ENCODING) {
-    // 11->00->11->00 is counter clockwise rotation or "forward".
+    // 11->00->11->00 is counter clockwise rotation or "forward" for left motor.
     if ((prev_state_[LEFT_ENC] == 0x3 && curr_state_[LEFT_ENC] == 0x0) ||
        (prev_state_[LEFT_ENC] == 0x0 && curr_state_[LEFT_ENC] == 0x3)) {
       pulses_[LEFT_ENC]++;
     } else {
-      // 10->01->10->01 is clockwise rotation or "backward".
+      // 10->01->10->01 is clockwise rotation or "backward" for left motor.
       if ((prev_state_[LEFT_ENC] == 0x2 && curr_state_[LEFT_ENC] == 0x1) ||
           (prev_state_[LEFT_ENC] == 0x1 && curr_state_[LEFT_ENC] == 0x2)) {
         pulses_[LEFT_ENC]--;
@@ -181,15 +187,15 @@ void QuadratureEncoder::encodeRight() {
 
   curr_state_[RIGHT_ENC] = (channel_a_state << 1) | (channel_b_state);  // 2-bit state.
   if (encoding_ == X2_ENCODING) {
-    // 11->00->11->00 is counter clockwise rotation or "forward".
+    // 11->00->11->00 is counter clockwise rotation or "backward" for right motor.
     if ((prev_state_[RIGHT_ENC] == 0x3 && curr_state_[RIGHT_ENC] == 0x0) ||
        (prev_state_[RIGHT_ENC] == 0x0 && curr_state_[RIGHT_ENC] == 0x3)) {
-      pulses_[RIGHT_ENC]++;
+      pulses_[RIGHT_ENC]--;
     } else {
-      // 10->01->10->01 is clockwise rotation or "backward".
+      // 10->01->10->01 is clockwise rotation or "forward" for right motor.
       if ((prev_state_[RIGHT_ENC] == 0x2 && curr_state_[RIGHT_ENC] == 0x1) ||
           (prev_state_[RIGHT_ENC] == 0x1 && curr_state_[RIGHT_ENC] == 0x2)) {
-        pulses_[RIGHT_ENC]--;
+        pulses_[RIGHT_ENC]++;
       }
     }
   } else {
@@ -198,7 +204,7 @@ void QuadratureEncoder::encodeRight() {
       if (((curr_state_[RIGHT_ENC] ^ prev_state_[RIGHT_ENC]) != INVALID) &&
           (curr_state_[RIGHT_ENC] != prev_state_[RIGHT_ENC])) {
         // 2 bit state. Right hand bit of prev XOR left hand bit of current
-        // gives 0 if clockwise rotation and 1 if counter clockwise rotation.
+        // gives 1 if clockwise rotation and 0 if counter clockwise rotation.
         change = (prev_state_[RIGHT_ENC] & PREV_MASK) ^ ((curr_state_[RIGHT_ENC] & CURR_MASK) >> 1);
         if (change == 0) change = -1;
         pulses_[RIGHT_ENC] += change;
